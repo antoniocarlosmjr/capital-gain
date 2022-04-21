9ad2e97747cf0479265df7a345861093d2f06d53
Antonio Martins <antonio.pereira@picpay.com>
2022-04-21 01:54:44 -0300

    feat: create calculators

---
+++ src/app/Application/Services/CalculatorTax.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Application\Services;
+
+class CalculatorTax
+{
+    private const PERCENT_IMPOST = 0.2;
+    private const LIMIT_VALUE_OF_OPERATION_WITHOUT_IMPOST = 20000;
+}
