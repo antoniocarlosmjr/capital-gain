a942ea7f32d4de2d673a1054a591aec872657490
Antonio Martins <antonio.pereira@picpay.com>
2022-04-21 01:59:26 -0300

    feat: update calculator of taxa

--- src/app/Application/Services/CalculatorTax.php
+++ src/app/Application/Services/CalculatorTax.php
@@ -2,8 +2,16 @@
 
 namespace App\Application\Services;
 
+use App\Domain\Entities\Operation\OperationEntity;
+
 class CalculatorTax
 {
     private const PERCENT_IMPOST = 0.2;
     private const LIMIT_VALUE_OF_OPERATION_WITHOUT_IMPOST = 20000;
+
+    public function calculate(OperationEntity $operationEntity)
+    {
+        // lembrar de verificar se é buy não paga taxa.
+        // se não for, paga taxa
+    }
 }
