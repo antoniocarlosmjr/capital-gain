a57b6e834c6590a36a98e9454db5a2218bea17dc
Antonio Martins <antonio.pereira@picpay.com>
2022-04-23 15:53:26 -0300

    test: create a test for factory

---
+++ src/tests/Unit/Infra/Factory/OperationFactoryTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit\Infra\Factory;
+
+use App\Domain\Entities\Operation\OperationArrayList;
+use App\Domain\Entities\Operation\OperationEntity;
+use App\Enumerators\TypesOperationEnum;
+use App\Infra\Factory\OperationFactory;
+use PHPUnit\Framework\TestCase;
+
+class OperationFactoryTest extends TestCase
+{
+    public function testCreateOperationListFromArray()
+    {
+        $operation = [
+            [
+                'operation' => TypesOperationEnum::BUY,
+                'unit-cost' => 10,
+                'quantity' => 10000
+            ]
+        ];
+
+        $operationFactory = new OperationFactory();
+        $operationArrayList = $operationFactory->createOperationFromArray($operation);
+        $expectedOperations = [
+            new OperationEntity(
+                TypesOperationEnum::BUY,
+                10,
+                10000
+            )
+        ];
+
+        self::assertIsObject($operationArrayList);
+        self::assertEquals($expectedOperations, $operationArrayList->getAllOperations());
+    }
+}
